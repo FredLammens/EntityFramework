2cd33ef548faadf59a21cb28309d9b46fd2dec83
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-05-12 19:12:11 +0200

    verder crud

---
+++ EntityFramework/CRUD/Data/StripContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD.Data
+{
+    class StripContext : DbContext
+    {
+        public DbSet<Strip> Strips { get; set; }
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<Reeks> Reeksen { get; set; }
+        public DbSet<Uitgeverij> Uitgeverijen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=boekDB;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuteurStrip>().HasKey(x => new { x.StripID, x.AuteurID });
+        }
+    }
+}
